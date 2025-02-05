692cbb4d1ab4a4b76aeb5b8da04294937717e757
ilyas <ilyasalqordhowi@gmail>
2025-02-05 12:28:42 +0700

    fix models

---
+++ dtos/auth.go
@@ -0,0 +1,13 @@
+package dtos
+
+type User struct {
+	Id        int    `json:"id"`
+	Email     string `json:"email" form:"email" db:"email" binding:"required,email"`
+	Password  string `form:"password" db:"password" binding:"required" json:"-"`
+	RoleId    int    `json:"roleId" form:"roleId" db:"role_id"`
+	ProfileId int    `json:"profileId" form:"profileId" db:"profile_id"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
