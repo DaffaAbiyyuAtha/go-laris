bbeb698d36268548b7cb68b80cd03de35c3ef391
ilyas <ilyasalqordhowi@gmail.com>
2025-03-26 10:06:58 +0700

    fix add total-transaction in database transaction

--- migrations/000006_transaction.up.sql
+++ migrations/000006_transaction.up.sql
@@ -1,5 +1,7 @@
 CREATE TABLE "transaction"(
     "id" serial primary key,
-    "date_transaction" TIMESTAMP,
+    "date_transaction" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "total_transaction" BIGINT,
+    "user_id" INT REFERENCES "user"("id"),
     "product_id" INT REFERENCES "product"("id")
 );
